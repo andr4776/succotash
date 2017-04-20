49762011e17ead021c82b59556fc1bf1d74ba636
andre roman <andre_roman_@outlook.com>
2017-04-20 19:44:36 +0100

    _v5

--- private.php
+++ private.php
@@ -32,7 +32,10 @@
   			<div class="container">
   				<div class="navbar-header wow fadeIn">
   					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
-  						 <button type="submit" class="btn btn-block btn-success"  onsubmit=" ">get me registered! </button>
+  						<span class="sr-only">Toggle navigation</span>
+  						<span class="icon-bar"></span>
+  						<span class="icon-bar"></span>
+  						<span class="icon-bar"></span>
   					</button>
   					<a class="navbar-brand" href=""></a>
   				</div>
@@ -52,6 +55,7 @@
   						<li><a href="profile.php">MyProfile</a></li>
   						<li><a href="vblog.php" >VBlog</a></li>
   						<li><a href="intouch.php">InTouch</a></li>
+              
   					</ul>
   				</div>
   			</div>
